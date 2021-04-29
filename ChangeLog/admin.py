74b744669a3c61a1dc5b34f934d31a248659f97f
Variiiest <varishgautamvari@gmail.com>
2021-04-30 03:46:46 +0530

    ChangeLog Created

--- ChangeLog/admin.py
+++ ChangeLog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ChangeLog
+admin.site.register(ChangeLog)
--- ChangeLog/models.py
+++ ChangeLog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class ChangeLog(models.Model):
+    type_of_change= models.CharField(max_length=50)
+    change= models.TextField()
+    change_time= models.TimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = ("ChangeLog")
+        verbose_name_plural = ("ChangeLogs")
+
+    def __str__(self):
+        return self.change
